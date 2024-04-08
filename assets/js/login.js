eed253744eaa8dab6db44b14a1cacd8a80830c48
MinhDat1312 <nguyenthangdat84@gmail.com>
2024-04-08 19:38:54 +0700

    update

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,62 +14,62 @@ function displayLogininRegister(){
     register.style.display="none";
 }
 
-function Register(e){
-    event.preventDefault();
-    var mail=document.getElementById('emailR').value;
-    var userName=document.getElementById('nameR').value;
-    var pass=document.getElementById('passR').value;
-    var repass=document.getElementById('repassR').value;
+// function Register(e){
+//     event.preventDefault();
+//     var mail=document.getElementById('emailR').value;
+//     var userName=document.getElementById('nameR').value;
+//     var pass=document.getElementById('passR').value;
+//     var repass=document.getElementById('repassR').value;
 
-    if(mail!="" && pass!="" && repass!="" && userName!=""){
-        if(pass==repass){
-            var user={
-                email: mail,
-                name: userName,
-                password: pass,
-                re_password: repass
-            };
+//     if(mail!="" && pass!="" && repass!="" && userName!=""){
+//         if(pass==repass){
+//             var user={
+//                 email: mail,
+//                 name: userName,
+//                 password: pass,
+//                 re_password: repass
+//             };
         
-            var json=JSON.stringify(user);
-            localStorage.setItem("username",json);
-            alert("Đăng ký thành công");
-            displayLogininRegister();
-        }
-        else{
-            alert("Mật khẩu không giống");
-        }
-    }
-    else{
-        alert("Vui lòng nhập thông tin");
-    }
+//             var json=JSON.stringify(user);
+//             localStorage.setItem("username",json);
+//             alert("Đăng ký thành công");
+//             displayLogininRegister();
+//         }
+//         else{
+//             alert("Mật khẩu không giống");
+//         }
+//     }
+//     else{
+//         alert("Vui lòng nhập thông tin");
+//     }
 
 
-    document.getElementById('emailR').value="";
-    document.getElementById('nameR').value="";
-    document.getElementById('passR').value="";
-    document.getElementById('repassR').value="";
-}
+//     document.getElementById('emailR').value="";
+//     document.getElementById('nameR').value="";
+//     document.getElementById('passR').value="";
+//     document.getElementById('repassR').value="";
+// }
 
-function Login(e){
-    event.preventDefault();
-    var userName=document.getElementById('name').value;
-    var password=document.getElementById('password').value;
+// function Login(e){
+//     event.preventDefault();
+//     var userName=document.getElementById('name').value;
+//     var password=document.getElementById('password').value;
 
-    if(userName=="" && password==""){
-        alert("Vui lòng nhập thông tin");
-    }
-    else{
-        var user=localStorage.getItem("username");
-        var data=JSON.parse(user);
+//     if(userName=="" && password==""){
+//         alert("Vui lòng nhập thông tin");
+//     }
+//     else{
+//         var user=localStorage.getItem("username");
+//         var data=JSON.parse(user);
 
-        if(user==null){
-            alert("Tài khoản chưa đăng ký");
-        }
-        else if(userName==data.name && password==data.password){
-            window.location.href="./assets/html/home.html";
-        }
-        else {
-            alert("Tài khoản chưa đăng ký");
-        }
-    }    
-}
+//         if(user==null){
+//             alert("Tài khoản chưa đăng ký");
+//         }
+//         else if(userName==data.name && password==data.password){
+//             window.location.href="./assets/html/home.html";
+//         }
+//         else {
+//             alert("Tài khoản chưa đăng ký");
+//         }
+//     }    
+// }
